5b3febc160b6d84566f5cee36fad102822cc7bb7
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-12-17 15:38:26 -0600

    p

--- clock/src/app/app.module.ts
+++ clock/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClockComponent } from './clock/clock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ClockComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
